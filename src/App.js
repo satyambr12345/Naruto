c38df4cd68fd72d0d548766b09d811a806857f23
Satyam Kumar <satyambr12345@gmail.com>
2023-08-14 13:07:19 +0530

    Le4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 function App() {
-  const title="Welcome to Naruto's Blogs"
+  const title="Welcome to Naruto's Blogs";
+  const likes=50;
+  const link="https://google.com"
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
+        <h3>Liked {likes} times</h3>
+        <p>{10}</p>
+        <p>{"Satyam"}</p>
+        <a href={link}>Google Site</a>
       </div>
     </div>
   );
